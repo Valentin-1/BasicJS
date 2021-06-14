c2d85527b7ad28feeab5410530f0bc426d0b9cdd
Valentin 1 <valikv03@gmail.com>
2021-06-14 10:03:28 +0300

    am facut exercitiul 1

---
+++ 1 SyntaxJS/Ex1-swich.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let number = prompt('Introduceti numarul')
+switch(number){
+	case '0':
+	alert('Null');
+	break;
+	case '1':
+	alert('Unu');
+	break;
+	case '2':
+	alert('Doi');
+	break;
+	case '3':
+	alert('Trei');
+	break;
+	case '4':
+	alert('Patru');
+	break;
+	case '5':
+	alert('Cinci');
+	break;
+	case '6':
+	alert('Sase');
+	break;
+	case '7':
+	alert('Sapte');
+	break;
+	case '8':
+	alert('Opt');
+	break;
+	case '9':
+	alert('Noua');
+	break;
+	default:
+	alert( 'Numarul nu a fost introdus corect' );
+}
