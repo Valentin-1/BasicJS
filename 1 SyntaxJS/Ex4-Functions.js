7b9c948e061a9a179097ddde614ba5d7cdf4874f
Valentin 1 <valikv03@gmail.com>
2021-06-14 14:36:28 +0300

    Schimbarea primei litere din cuvant pe majuscula

---
+++ 1 SyntaxJS/Ex4-Functions.js
@@ -0,0 +1,5 @@
+let ucFirst = function (str){
+	alert(`Cuvantul initial: ${str}
+Cuvantul schimbat cu prima litera mare: ${str[0].toUpperCase() + str.slice(1)}`);
+}
+ucFirst(prompt('Introduceti cuvantul cu litere mici'));
