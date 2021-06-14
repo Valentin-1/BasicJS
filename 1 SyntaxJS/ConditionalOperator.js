9ca6b022bafec9a37bb1e1c470f73826fc03696b
Valentin 1 <valikv03@gmail.com>
2021-06-14 13:50:47 +0300

    Afisarea numarului maxim dintr-un sir

---
+++ 1 SyntaxJS/ConditionalOperator.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let n = prompt('Introduce-ti numarul ', '');
+let message = (n == 0) ? 'null' :
+(n == 1) ? 'unu':
+(n == 2) ? 'doi':
+(n == 3) ? 'trei':
+(n == 4) ? 'patru':
+(n == 5) ? 'cinci':
+(n == 6) ? 'sase':
+(n == 7) ? 'sapte':
+(n == 8) ? 'opt':
+(n == 9) ? 'noua':
+'Numarul introdus nu corespunde cerintei';
+
+alert (message);
+
