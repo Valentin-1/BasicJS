36d15d900a713c52dfae8e833527e97da2a84648
Valentin 1 <valikv03@gmail.com>
2021-07-07 17:10:41 +0300

    Am revizuit codul si am schimbat unele greseli

--- 2 ArraysJS/Ex3-ArrayProcessing.js
+++ 2 ArraysJS/Ex3-ArrayProcessing.js
@@ -7,25 +7,16 @@ styles[2] = 'Рок-н-рол'
 console.log(styles);
 
 //3)Adugarea si inlacuirea 
-let arraySplice = styles
-n = prompt('Indicati pozitia elemetului care trebuie schimbat')
-m = prompt('Indicati cate elemente vor fi schimbate')
-//w = prompt('Indicati cuvantul')
-styles.splice(n, m, 'Классика');
-console.log(arraySplice);
+styles.splice(1, 1, 'Классика');
+console.log(styles);
 
 //4)Stergerea unui element
-let elemDelete = arraySplice;
-elemDelete.splice(0, 1);
-console.log(elemDelete);
-
-console.log('Afisarea elementului sters, dupa idee trebuie sa lucreze metoda asta, dar mie imi afisazea primul element');
-let remove = arraySplice.splice(0, 1);
-console.log(remove);
+styles.shift();
+console.log(styles);
 
 //5)Adaugarea elementelor la inceputul sirului
-elemDelete.unshift('Рэп', 'Регги');
-console.log(elemDelete);
+styles.unshift('Рэп', 'Регги');
+console.log(styles);
 
 
 
